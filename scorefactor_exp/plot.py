96112b453aeb68b6dd693900c2c9a9186992f1d6
Ali <aaldakhe@andrew.cmu.edu>
2020-11-02 00:09:43 -0500

    new parsing; updated plotting; added results

--- scorefactor_exp/plot.py
+++ scorefactor_exp/plot.py
@@ -2,8 +2,10 @@
 Plot results of experiment
 """
 
+import matplotlib as matplt
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 def level_vs_time_subplots(dfs, conf):
     cols = 1
@@ -14,6 +16,7 @@ def level_vs_time_subplots(dfs, conf):
                 x='time',
                 y='level',
                 data=df,
+                linestyles='',
                 scale=0.4,
                 errwidth=0.3,
                 ci='sd',
@@ -29,8 +32,140 @@ def level_vs_time_subplots(dfs, conf):
     fig.subplots_adjust(left=0.08, right=0.94, bottom=0.1, top=0.94, hspace=0.2)
     plt.show()
 
+def level_vs_time(df, conf):
+    markers = ['x', 'o', '^', 'v']
+    # for c, marker in zip(conf, markers):
+        # df_conf = df[df.config == c]
+        # g = sns.pointplot(
+                # y='level', x='time', hue='cnf', data=df_conf,
+                # ci='sd',
+                # scale=0.6,
+                # markers=marker,
+                # palette='Set2',
+                # dodge=True
+        # )
+        # g.set_xticklabels(g.get_xticklabels(),
+                # fontsize=6, rotation=43, ha="right")
+        # g.set_xlabel("Time (s)")
+        # g.set_ylabel("Level")
+    df['config_sat'] = df.config.str.cat(df.sat_state, sep=', ').astype(str)
+    g = sns.lineplot(
+            x='level', y='time', hue='cnf', data=df[:100000],
+            ci='sd',
+            markers=True,
+            dashes=False,
+            err_style='bars',
+            # scale=0.8,
+            # errwidth=0.6,
+            # markers='*',
+            style='config',
+            # style='config_sat',
+            # style='sat_state',
+            # palette='Set2',
+            palette='Dark2',
+            # dodge=True
+    )
+    # g.set_xticklabels(g.get_xticklabels(),
+            # fontsize=6, rotation=43,
+            # # ha="right"
+            # )
+    g.set_xlabel("Time (s)")
+    g.set_ylabel("Level")
+    g.legend('')
+    plt.show()
+
+def level_vs_time_sp2(df, conf):
+    markers = ['x', 'o', '^', 'v']
+    cols = 1
+    fig, ax_arr = plt.subplots(len(conf)//cols, cols, sharex=True)
+    ax_arr.flatten()
+    for i, (c, marker) in enumerate(zip(conf, markers)):
+        df_conf = df[df.config == c]
+        g = sns.pointplot(
+                y='level', x='time', hue='cnf', data=df_conf,
+                ci='sd',
+                scale=0.6,
+                errwidth=0.5,
+                markers=marker,
+                # palette=sns.color_palette('Spectral'),
+                palette='tab20',
+                ax=ax_arr[i]
+        )
+        g.legend('')
+        ax = ax_arr[i]
+        ax.set_xticklabels(ax.get_xticklabels(),
+                fontsize=6, rotation=43, ha="right")
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+        ax.set_title(conf[i].split('___')[-1])
+    fig.text(0.5, 0.04, "Time (s)", va='center', ha='center',)
+    fig.text(0.04, 0.5, "Level (mean, std)", va='center', ha='center', rotation='vertical')
+    fig.subplots_adjust(left=0.08, right=0.94, bottom=0.1, top=0.94, hspace=0.2)
+    plt.show()
+
+def level_vs_time_diff(df, conf):
+    # markers = ['x', 'o', '^', 'v']
+    markers = [i for i in range(12)]
+    # cm = sns.color_palette('tab20')
+    cm = sns.color_palette('twilight', len(df.cnf.unique()))
+    num_plot = np.math.factorial(len(conf)) // (2 * np.math.factorial(len(conf)-2))
+    cnfs = df.cnf.unique()
+
+    cols = 1
+    fig, ax_arr = plt.subplots(num_plot//cols, cols, sharex=True)
+    ax_arr.flatten()
+    for i, c in enumerate(conf):
+        df_conf = df[df.config == c].reset_index()
+        for c2 in enumerate(conf[i+1:]):
+            df_conf2 = df[df.config == c2].reset_index()
+            df_conf["time_diff"] = abs(df_conf.time - df_conf2.time)
+            g = sns.pointplot(
+                    y='level', x='time', hue='cnf', data=df_conf,
+                    ci='sd',
+                    scale=0.6,
+                    errwidth=0.5,
+                    # markers=marker,
+                    palette=cm,
+                    hue_order=cnfs,
+                    ax=ax_arr[i]
+            )
+            g.legend('')
+            ax = ax_arr[i]
+            ax.set_xticklabels(ax.get_xticklabels(),
+                    fontsize=6, rotation=43, ha="right")
+            ax.set_xlabel("")
+            ax.set_ylabel("")
+            ax.set_title(conf[i].split('___')[-1])
+    fig.text(0.5, 0.04, "Time (s)", va='center', ha='center',)
+    fig.text(0.04, 0.5, "Level (mean, std)", va='center', ha='center', rotation='vertical')
+    fig.subplots_adjust(left=0.08, right=0.94, bottom=0.1, top=0.94, hspace=0.2)
+    plt.show()
+
+def level_vs_time_boxplots(df, conf):
+    cols = 2
+    num_plot = 4
+    fig, ax_arr = plt.subplots(num_plot//cols, cols, sharex=True)
+    for i, c in enumerate(['time', 'av_level']):
+        for i2, ss in enumerate(df.sat_state.unique()):
+            ax = ax_arr[i2, i]
+            df_conf = df[df.sat_state == ss].sort_values('config')
+            g = sns.boxplot(x='config', y=c, data=df_conf,
+                    showmeans=True,
+                    ax=ax
+            )
+
+            ax.set_xticklabels(
+                    [str(b).rstrip(')').rstrip('\'').split('___')[-1]
+                        for b in ax.get_xticklabels()],
+                    fontsize=15, rotation=20, ha="right")
+            ax.set_xlabel("")
+            ax.set_title(f"{c}, {ss}")
+    fig.subplots_adjust(left=0.08, right=0.94, bottom=0.1, top=0.94, hspace=0.2)
+    plt.show()
+
 if __name__ == "__main__":
-    from parse_output import parse_exp_output, dict_to_dfs
+    # from parse_output import parse_exp_output, dict_to_dfs
+    from parse_output2 import parse_exp_output, dict_to_df
     exp_outputs_dir = "exp_output"
     config_list = [
             "cadical-1.3.1-c9b8d0b67a123___sat_500",
@@ -39,7 +174,21 @@ if __name__ == "__main__":
             "cadical-1.3.1-c9b8d0b67a123___unsat_950"
             ]
     res_dict = parse_exp_output(exp_outputs_dir, config_list)
-    dfs = dict_to_dfs(res_dict)
-    level_vs_time_subplots(dfs, config_list)
+    # dfs = dict_to_dfs(res_dict)
+    # df = dict_to_df(res_dict)
+    df = dict_to_df(res_dict, explode_level=False)
+
+    # filter_outliers
+    # outlier_threshold = 1000 #(level)
+    # for i, df in enumerate(dfs):
+        # df.level = df.level.astype(int)
+        # dfs[i] = df.groupby('path').filter(lambda x: x['level'].mean() < outlier_threshold)
+
+    # level_vs_time_subplots(dfs, config_list)
+
+    # level_vs_time(df, config_list)
+    # level_vs_time_sp2(df, config_list)
+    # level_vs_time_diff(df, config_list)
+    level_vs_time_boxplots(df, config_list)
 
 
